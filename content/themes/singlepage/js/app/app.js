addd1c340a893f952954ac0bdd0994db8bd85034
Daniel Lamprecht <daniel.lamprecht@gmx.at>
2016-11-22 22:05:32 +0100

    add example graph

--- content/themes/singlepage/js/app/app.js
+++ content/themes/singlepage/js/app/app.js
@@ -1,54 +1,46 @@
 define(['controllers', 'directives', 'filters', 'lodash'], function () {
   'use strict';
-  var app = angular.module('app', [ 'app.controllers', 'app.directives', 'app.filters', 'chart.js', 'ui.bootstrap', 'ui.router', 'ngSanitize', 'smoothScroll', 'ui-notification', 'rzModule', 'treeControl', 'ngAnimate', 'angular-loading-bar', 'LocalStorageModule']);
+  var app = angular.module('app', ['app.controllers', 'app.directives', 'app.filters', 'chart.js', 'ui.bootstrap', 'ui.router', 'ngSanitize', 'smoothScroll', 'ui-notification', 'rzModule', 'treeControl', 'ngAnimate', 'angular-loading-bar', 'LocalStorageModule']);
 
   app.config(['$locationProvider', '$stateProvider', function ($locationProvider, $stateProvider) {
     $locationProvider.html5Mode({
-          enabled: true,
-          rewriteLinks: false
-      });
-    
-    $stateProvider.state('detail', { 
-        templateUrl: '/modal.html',
-        url: '/detail/:archivnr',
-        controller: 'SearchModalController',
-        resolve:{
-            hit: ['config', '$http','$location', '$q', function(config, $http, $location, $q){
-              var deferred = $q.defer();
-              $http.get(config.solrBaseUrl + '/solr/work/select', {
-                    params: {
-                        'q' : 'archivnr:' + $location.search().archivnr,
-                        'wt': 'json'
-                      }
-                    }).success(function(searchResultResponse) {
-                      
-                      deferred.resolve(searchResultResponse.response.docs[0]);
-                      
-                    }).error(function(content) {
-                      if(content){
-                        console.log('Error: ' + content);
-                      }
-                    });
-              return deferred.promise;
-            }],
-            descriptions: ['hit', 'productservice', function(hit, productservice){
-              return productservice.retrieveDescriptions(hit);
-            }],
-            ensembles: ['hit', 'productservice', function(hit, productservice){
-              return productservice.retrieveEnsembles(hit);
-            }],
-            data: ['hit', 'productservice', function(hit, productservice){
-              return productservice.retrieveData(hit);
-            }],
-            searchkeyword: function(){
-                return '';
-            },
-            hideClose: function(){
-              return true;
+      enabled: true,
+      rewriteLinks: false
+    });
+
+    var detailOptions = {
+      template: '<div class="d3-graph"> </div>',
+      url: '/graphtest/',
+      controller: 'GraphController',
+      resolve: {
+        hit: ['config', '$http', '$location', '$q', function (config, $http, $location, $q) {
+          var deferred = $q.defer();
+          $http.get(config.solrBaseUrl + '/solr/work/select', {
+            params: {
+              'q': 'archivnr:000168',
+              'wt': 'json'
             }
-        }
-     });
-    
+          }).success(function (searchResultResponse) {
+
+            deferred.resolve(searchResultResponse.response.docs[0]);
+
+          }).error(function (content) {
+            if (content) {
+              console.log('Error: ' + content);
+            }
+          });
+          return deferred.promise;
+        }],
+          data: ['hit', 'productservice', function (hit, productservice) {
+          return productservice.retrieveData(hit);
+        }]
+      }
+    };
+    $stateProvider.state('detail', detailOptions);
+
+    // detailOptions.url = '/en/graphtest/';
+    // $stateProvider.state('detail-en', detailOptions);
+
   }]);
 
   app.factory('sessionData', function () {
@@ -65,40 +57,40 @@ define(['controllers', 'directives', 'filters', 'lodash'], function () {
   })
 
   app.constant('config', {
-      //solrBaseUrl: 'http://localhost:8983'
-      solrBaseUrl: 'http://realonline.imareal.sbg.ac.at',
-      //solrBaseUrl: 'http://104.45.88.196'
-      // solrBaseUrl: 'http://realonline.imareal.sbg.ac.at.local'
-      apiBaseUrl: 'http://cf000044.sbg.ac.at/api/data/',
-      apiKey: '9rkWCL6e86',
-      imageBaseUrl: 'http://cf000036.sbg.ac.at/iipsrv/iipsrv.fcgi?FIF=/'
+    //solrBaseUrl: 'http://localhost:8983'
+    solrBaseUrl: 'http://realonline.imareal.sbg.ac.at',
+    //solrBaseUrl: 'http://104.45.88.196'
+    // solrBaseUrl: 'http://realonline.imareal.sbg.ac.at.local'
+    apiBaseUrl: 'http://cf000044.sbg.ac.at/api/data/',
+    apiKey: '9rkWCL6e86',
+    imageBaseUrl: 'http://cf000036.sbg.ac.at/iipsrv/iipsrv.fcgi?FIF=/'
   });
 
   angular.bootstrap(document, ['app'], {
     strictDi: true
   });
 
-  if(angular.element('.billing-container').length){
+  if (angular.element('.billing-container').length) {
     var publikation = angular.element('#publikation');
-    var modifyOption = function(element) {
+    var modifyOption = function (element) {
       var publikation_visible = publikation.val() === 'Ja' ? 'block' : 'none';
       element.val(publikation.val() === 'Ja' ? '' : '-');
-      if(publikation.val() === 'Ja'){
+      if (publikation.val() === 'Ja') {
         element.parent().show();
-      }else{
+      } else {
         element.parent().hide();
       }
     };
-    var modifyOptions = function() {
+    var modifyOptions = function () {
       modifyOption(angular.element('#publikation_author'));
       modifyOption(angular.element('#publikation_title'));
       modifyOption(angular.element('#publikation_verlag'));
       modifyOption(angular.element('#publikation_auflage'));
     }
     modifyOptions();
-    
+
     publikation.on('change', modifyOptions);
-    
+
   }
 
 });
--- content/themes/singlepage/js/app/controllers.js
+++ content/themes/singlepage/js/app/controllers.js
@@ -1,6 +1,7 @@
 define('controllers', [
   './controllers/module',
   './controllers/searchcontroller',
+  './controllers/graphcontroller',
   './controllers/collectioncontroller',
   './controllers/quellemodalcontroller'
 ]);
